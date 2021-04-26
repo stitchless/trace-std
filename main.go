def85db622898c1bc0d43065cc2e652b79a832f4
Josh Peizer <github@sent.sh>
2021-04-26 13:51:32 -0400

    Streamlined a bit more

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
+
 import (
-	"log"
-	"os"
-	"os/exec"
+    "bytes"
+    "io"
+    "log"
+    "os"
+    "os/exec"
 )
 func main() {
-	arg := os.Args[1]
-	command := exec.Command(arg)
-	command.Stdout = os.Stdout
-	command.Stderr = os.Stderr
-	err := command.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = command.Wait()
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Binary to pall to the function
+    arg := os.Args[1]
+
+    // assign binary as child process
+    command := exec.Command(arg)
+
+    var stdBuffer bytes.Buffer
+    mw := io.MultiWriter(os.Stdout, &stdBuffer)
+
+    command.Stdout = mw
+    command.Stderr = mw
+
+
+    // Run the binary and stream the output.
+    if err := command.Run(); err != nil {
+        log.Panicln(err)
+    }
+
+    // Stream the log output to the terminal window
+    log.Println(stdBuffer.String())
 }
