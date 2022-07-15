98cf63241e3e3467e2f37f170d8b50be15089911
Josh Peizer <github@sent.sh>
2022-07-15 11:04:19 -0400

    Renamed args to cmd and added an additional args field

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-    "bytes"
-    "io"
-    "log"
-    "os"
-    "os/exec"
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"os/exec"
 )
+
 func main() {
-    // Binary to pall to the function
-    arg := os.Args[1]
+	// Binary to pall to the function
+	cmd := os.Args[1]
+	params := os.Args[2]
 
-    // assign binary as child process
-    command := exec.Command(arg)
+	// assign binary as child process
+	command := exec.Command(cmd, params)
 
-    var stdBuffer bytes.Buffer
-    mw := io.MultiWriter(os.Stdout, &stdBuffer)
+	var stdBuffer bytes.Buffer
+	mw := io.MultiWriter(os.Stdout, &stdBuffer)
 
-    command.Stdout = mw
-    command.Stderr = mw
+	command.Stdout = mw
+	command.Stderr = mw
 
+	// Run the binary and stream the output.
+	if err := command.Run(); err != nil {
+		log.Panicln(err)
+	}
 
-    // Run the binary and stream the output.
-    if err := command.Run(); err != nil {
-        log.Panicln(err)
-    }
-
-    // Stream the log output to the terminal window
-    log.Println(stdBuffer.String())
+	// Stream the log output to the terminal window
+	log.Println(stdBuffer.String())
 }
